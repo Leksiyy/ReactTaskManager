6094d8faba75fefbc28f9bd41068462e4e3e183c
Leksiyy <leksiy197618@gmail.com>
2025-03-14 11:16:13 +0200

    floatbutton settings and settings modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import './App.css'
 import {Layout} from "antd";
 import {useState} from "react";
 import CardsContainer from "./components/CardsContainer.tsx";
 import HeaderComponent from "./components/HeaderComponent.tsx";
+import Settings from "./components/Settings.tsx";
 
 function App() {
     const [position, setPosition] = useState<'Current' | 'Archive'>('Current');
 
 
-
-
     return (
         <Layout style={{ minHeight: '100vh' }}>
 
-            <HeaderComponent position={position} setPosition={setPosition} /> {/* Используем хедер */}
+            <HeaderComponent position={position} setPosition={setPosition} />
 
             <CardsContainer />
+
+            <Settings />
 
         </Layout>
   )
