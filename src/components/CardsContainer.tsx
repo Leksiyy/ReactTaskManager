e44e1b4612825ecdcc5082ba75cc68125901d3cf
Denis <bigdramashow730@gmail.com>
2025-03-15 01:57:01 +0200

    save changes cardcontainer test

--- src/components/CardsContainer.tsx
+++ src/components/CardsContainer.tsx
@@ -1,5 +1,43 @@
 import { Content } from "antd/es/layout/layout";
+import { TaskCard } from "./TaskCard";
 
+const Tasks = [
+    {
+        id: "1",
+        title: "card title 1",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+        createdAt: new Date().toLocaleDateString(),
+        updatedAt: new Date().toLocaleDateString(),
+        tags: ["zxc", "test", "qwe"]
+    },
+    {
+        id: "2",
+        title: "card title 2",
+        text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium ",
+        createdAt: new Date().toLocaleDateString(),
+        updatedAt: new Date().toLocaleDateString(),
+        tags: ["23", "12", "gh"]
+    },
+]
+
+
+
+// Потом вынести методы?
+const handleEdit = (id: string) => {
+    console.log("Edit task", id);
+};
+
+const handleDelete = (id: string) => {
+    console.log("Delete task", id);
+};
+
+const handleComplete = (id: string) => {
+    console.log("Complete task", id);
+};
+
+const handleRestore = (id: string) => {
+    console.log("Restore task", id);
+};
 
 const CardsContainer = () => {
     return (
@@ -26,6 +64,16 @@ const CardsContainer = () => {
                 }}
             >
                 {/* Здесь будут карточки */}
+                                {Tasks.map(task => 
+                    <TaskCard 
+                        key={task.id}
+                        task={task}
+                        onEdit={handleEdit} 
+                        onDelete={handleDelete} 
+                        onComplete={handleComplete} 
+                        onRestore={handleRestore}
+                    />
+                )}
             </div>
         </Content>
     );
